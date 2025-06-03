8afd739aeb45388753103d121bc5f50a9ef205f1
hayeon00 <rlagkdus1024@naver.com>
2025-06-03 21:06:50 +0900

    fix: 메인화면에서 페이지 이동

--- requirements/frontend/src/App.jsx
+++ requirements/frontend/src/App.jsx
@@ -1,8 +1,11 @@
+// 이 위에 아무것도 두지 마세요 — 공백 줄, 주석 등
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import MainPage from './pages/MainPage';
 import MyPage from './pages/MyPage';
 import ReviewPage from './pages/ReviewPage';
+import ReviewPage from './pages/ReviewPage';
+import MyPage from './pages/MyPage';
 
 function App() {
     return (
--- requirements/frontend/src/pages/MainPage.jsx
+++ requirements/frontend/src/pages/MainPage.jsx
@@ -13,14 +13,13 @@ const MainPage = () => {
                     {/* <li onClick={() => navigate('/search')}>영화 검색</li> */}
                     <li onClick={() => navigate('/review')}>리뷰 쓰기</li>
                     {/* <li onClick={() => navigate('/popular')}>인기 영화</li>*/}
-                    {/*<li onClick={() => navigate('/mypage')}>마이페이지</li> */}
-                    {/*<li className="logout" onClick={() => navigate('/')}>로그아웃</li> */}
+                    <li onClick={() => navigate('/mypage')}>마이페이지</li> 
+                    <li className="/" onClick={() => navigate('/')}>로그아웃</li>
                 </ul>
             </nav>
 
             <div className="welcome">
-                <h1>환영합니다! 👋</h1>
-                <p>지금 가장 핫한 영화를 검색하고 리뷰를 남겨보세요.</p>
+
 
             </div>
         </div>
