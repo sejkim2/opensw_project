a86a20af872e245452241b50825faf5b5557332a
hayeon00 <rlagkdus1024@naver.com>
2025-06-03 21:06:50 +0900

    fix: 메인화면에서 페이지 이동

--- requirements/frontend/src/App.jsx
+++ requirements/frontend/src/App.jsx
@@ -1,28 +1,20 @@
+// 이 위에 아무것도 두지 마세요 — 공백 줄, 주석 등
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import MainPage from './pages/MainPage';
-<<<<<<< HEAD
 import ReviewPage from './pages/ReviewPage';
-=======
 import MyPage from './pages/MyPage';
->>>>>>> origin/master
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
-            <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/main" element={<MainPage />}/>
-<<<<<<< HEAD
-                <Route path="/review" element={<ReviewPage />}/>
-
-=======
-                <Route path="/MyPage" element={<MyPage />}/>
->>>>>>> origin/master
-
-            </Routes>
-
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/main" element={<MainPage />} />
+                    <Route path="/review" element={<ReviewPage />} />
+                    <Route path="/MyPage" element={<MyPage />} />
+                </Routes>
             </div>
         </BrowserRouter>
     );
