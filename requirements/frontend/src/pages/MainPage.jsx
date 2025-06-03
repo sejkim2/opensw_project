5fa2fbd4246f70dd34bba3f38da5eddf8188ac66
sejkim2 <128696540+sejkim2@users.noreply.github.com>
2025-06-03 21:17:45 +0900

    Revert "Feature/mainpage"

--- requirements/frontend/src/pages/MainPage.jsx
+++ requirements/frontend/src/pages/MainPage.jsx
@@ -1,29 +1,7 @@
-// src/MainPage.js
-import { useNavigate } from 'react-router-dom';
-import "./MainPage.css";
-
-const MainPage = () => {
-    const navigate = useNavigate();
-
-    return (
-        <div className="main-container">
-            <nav className="navbar">
-                <h2 className="logo">🎬 리뷰플랫폼</h2>
-                <ul className="menu">
-                    {/* <li onClick={() => navigate('/search')}>영화 검색</li> */}
-                    <li onClick={() => navigate('/review')}>리뷰 쓰기</li>
-                    {/* <li onClick={() => navigate('/popular')}>인기 영화</li>*/}
-                    <li onClick={() => navigate('/mypage')}>마이페이지</li> 
-                    <li className="/" onClick={() => navigate('/')}>로그아웃</li>
-                </ul>
-            </nav>
-
-            <div className="welcome">
 
 
-            </div>
-        </div>
-    );
-};
+function Main() {
+    return <h1 style={{ textAlign: "center" }}>메인 페이지입니다</h1>;
+}
 
-export default MainPage;
+export default Main;
