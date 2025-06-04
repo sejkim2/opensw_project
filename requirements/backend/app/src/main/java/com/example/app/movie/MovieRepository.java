4b1661e8b0139dad80ae0ce29edc9fbfbfc3f655
sejkim2 <kimsejin2118@gmail.com>
2025-06-04 19:39:56 +0900

    add : 사용자가 선호하는 장르와 영화 테이블 조인하여 반환

--- requirements/backend/app/src/main/java/com/example/app/movie/MovieRepository.java
+++ requirements/backend/app/src/main/java/com/example/app/movie/MovieRepository.java
@@ -1,6 +1,15 @@
 package com.example.app.movie;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.app.genre.Genre;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    @Query("SELECT DISTINCT m FROM Movie m JOIN m.genres g WHERE g IN :genres")
+    List<Movie> findByGenresIn(@Param("genres") Set<Genre> genres);
 }
